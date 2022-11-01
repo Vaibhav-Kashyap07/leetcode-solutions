4367828f6142d46203f3e41344d37825d2c82772
Vaibhav Kashyap <116161030+Vaibhav-Kashyap07@users.noreply.github.com>
2022-11-01 21:09:56 +0530

    Update valid-anagram.py

--- Python/valid-anagram.py
+++ Python/valid-anagram.py
@@ -1,3 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t): return False
+
+        CharCountS, CharCountT = {}, {}
+
+        for i in range(len(s)):
+            CharCountS[s[i]] = 1 + CharCountS.get(s[i], 0)
+            CharCountT[t[i]] = 1 + CharCountT.get(t[i], 0)
+        return CharCountS == CharCountT
